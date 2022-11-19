4bf7f974f47c314813127c37e97f2ae0a7052bdf
BibekRajPaudel <paudelrajbibek@gmail.com>
2022-11-19 22:41:09 +0545

    adding new forms for user and notes

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,6 +1,5 @@
-import { configureStore } from "@reduxjs/toolkit"
-import { apiSlice } from './api/apiSlice'
-import { setupListeners } from "@reduxjs/toolkit/query"
+import { configureStore } from "@reduxjs/toolkit";
+import { apiSlice } from './api/apiSlice';
 
 export const store = configureStore({
     reducer: {
@@ -10,5 +9,3 @@ export const store = configureStore({
         getDefaultMiddleware().concat(apiSlice.middleware),
     devTools: true
 })
-
-setupListeners(store.dispatch)
